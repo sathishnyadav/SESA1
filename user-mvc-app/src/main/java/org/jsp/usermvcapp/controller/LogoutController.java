1112cb3ad32991e056b3a8760736bbf7b8ebb9be
sathishnyadav <sathish.n.yadav@gmail.com>
2023-05-26 15:45:58 +0530

    added basic Spring MVC APP

---
+++ user-mvc-app/src/main/java/org/jsp/usermvcapp/controller/LogoutController.java
@@ -0,0 +1,19 @@
+package org.jsp.usermvcapp.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LogoutController {
+	@RequestMapping("/logout")
+	public ModelAndView logout(HttpSession session) {
+		session.invalidate();
+		ModelAndView view = new ModelAndView();
+		view.setViewName("login.jsp");
+		view.addObject("msg", "you are Logged out");
+		return view;
+	}
+}
---
+++ user-mvc-app/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+	<h2>Hello World!</h2>
+	<h2>
+		<a href="register.jsp">Click here to Register</a>
+	</h2>
+</body>
+</html>
